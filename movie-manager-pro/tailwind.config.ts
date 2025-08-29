9653efc2ebc9dada7ea3f81b223c604fc3fa1865
garimabhayanaa <garimaabhayanaa@gmail.com>
2025-08-30 02:29:52 +0530

    Initialize Next.js project with TypeScript and Tailwind CSS

---
+++ movie-manager-pro/tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e40202",
+        secondary: "#cd0a0a",
+        accent: "#737373",
+        dark: "#111010",
+      },
+      animation: {
+        'stripes': 'stripes 1s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
